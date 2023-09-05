bfa3f9bd0c6541391682aed87f9138effaa0404e
EC2 Default User <ec2-user@ip-172-31-12-31.ap-northeast-1.compute.internal>
2023-09-05 09:15:38 +0000

    feat:使用コントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'tags/index'
+  end
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+  end
+  namespace :public do
+    get 'tags/index'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :public do
+    get 'posts/index'
+    get 'posts/new'
+    get 'posts/show'
+    get 'posts/edit'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
