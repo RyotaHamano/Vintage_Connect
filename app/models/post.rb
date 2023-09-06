ed46bb0a27c925754797271faac6d6f9607c9852
EC2 Default User <ec2-user@ip-172-31-12-31.ap-northeast-1.compute.internal>
2023-09-06 06:21:18 +0000

    feat:アソシエーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ApplicationRecord
   
-  has_many :taggings
+  has_many_attached :post_images
+  
+  has_many :taggings, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
+  belongs_to :user
+  
   
   
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,6 @@
 class Tag < ApplicationRecord
+  
+  belongs_to :user
+  has_many :taggings, dependent: :destroy
+  
 end
