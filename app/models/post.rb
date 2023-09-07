14d5c815cf34a5021bf9cad3901b77a5a407a987
EC2 Default User <ec2-user@ip-172-31-12-31.ap-northeast-1.compute.internal>
2023-09-07 05:53:34 +0000

    feat:YMLファイル追加/i18n/enum定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,10 +7,12 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
   
-  enum prefecture: {Hokkaido: 0, Aomori: 1, Iwate: 2, Miyagi: 3, Akita: 4, Yamagata: 5, Fukushima: 6, Ibaraki: 7, Tochigi: 8, Gunma: 9, Saitama: 10, 
-                    Chiba: 11, Tokyo: 12, Kanagawa: 13, Niigata: 14, Toyama: 15, Ishikawa: 16, Fukui: 17, Yamanashi: 18, Nagano: 19, Gifu: 20, 
-                    Shizuoka: 21, Aichi: 22, Mie: 23, Shiga: 24, Kyoto: 25, Osaka: 26, Hyogo: 27, Nara: 28, Wakayama: 29, Tottori: 30, 
-                    Shimane:31, Okayama: 32, Hiroshima: 33, Yamaguchi: 34, Tokushima: 35, Kagawa: 36, Ehime: 37, Kochi: 38, Fukuoka: 39, Saga: 40, 
-                    Nagasaki: 41, Kumamoto: 42, Oita: 43, Miyazaki: 44, Kagoshima: 45, Okinawa: 46}
+  enum prefecture: {hokkaido: 0, aomori: 1, iwate: 2, miyagi: 3, akita: 4, yamagata: 5, fukushima: 6, ibaraki: 7, tochigi: 8, gunma: 9, saitama: 10, 
+                    chiba: 11, tokyo: 12, kanagawa: 13, niigata: 14, toyama: 15, ishikawa: 16, fukui: 17, yamanashi: 18, nagano: 19, gifu: 20, 
+                    shizuoka: 21, aichi: 22, mie: 23, shiga: 24, kyoto: 25, osaka: 26, hyogo: 27, nara: 28, wakayama: 29, tottori: 30, 
+                    shimane:31, okayama: 32, hiroshima: 33, yamaguchi: 34, tokushima: 35, kagawa: 36, ehime: 37, kochi: 38, fukuoka: 39, saga: 40, 
+                    nagasaki: 41, kumamoto: 42, oita: 43, miyazaki: 44, kagoshima: 45, okinawa: 46}
+  
+  enum shop_genre: {old_clothing: 0, old_item: 1, old_tool: 2, old_interior: 3, antique: 4}
   
 end
