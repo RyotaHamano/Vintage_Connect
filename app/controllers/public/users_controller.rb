a3c4a371c2cb8b54e0a6e2fe62db0c2df4910740
EC2 Default User <ec2-user@ip-172-31-12-31.ap-northeast-1.compute.internal>
2023-09-10 07:47:13 +0000

    feat:ユーザーページ/ユーザー情報編集ページ

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,16 +3,20 @@ class Public::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.where(reading_status: false)
-   
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
   
   def confirm
+    @user = User.find(params[:id])
   end
   
   def destroy
@@ -22,6 +26,12 @@ class Public::UsersController < ApplicationController
   end
   
   def followeds
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :most_favorite, :user_image, :introduction)
   end
   
 end
