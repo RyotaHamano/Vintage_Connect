55e9db21d10bb54c03070d500e8b4ef426faf939
EC2 Default User <ec2-user@ip-172-31-12-31.ap-northeast-1.compute.internal>
2023-09-19 06:23:38 +0000

    feat:フォロー機能非同期化

--- app/controllers/public/relations_controller.rb
+++ app/controllers/public/relations_controller.rb
@@ -4,14 +4,12 @@ class Public::RelationsController < ApplicationController
   
   def create
     current_user.follow.create(followed_id: params[:user_id])
-    @user = User.find(params[:user_id])
-    
+    @follow_user = User.find(params[:user_id])
   end
   
   def destroy
     current_user.follow.find_by(followed_id: params[:user_id]).destroy
-    @user = User.find(params[:user_id])
-    redirect_to request.referer
+    @follow_user = User.find(params[:user_id])
   end
   
 end
